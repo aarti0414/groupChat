b6bb4d09815b89d28e6f2120bacf6af648ee1a9f
aarti0414 <apwaraikar0403@gmail.com>
2023-03-22 12:08:00 +0530

    redirect successful login to chat app page

--- frontend/script.js
+++ frontend/script.js
@@ -63,6 +63,8 @@ function login(e) {
       if (response.status === 200) {
         localStorage.setItem('token', response.data.token);
         localStorage.setItem('name', response.data.name);
+        document.cookie = `token=${response.data.token}`;
+        window.location.href = 'chat.html'
       }
     }).catch(error => {
       console.log(error);
